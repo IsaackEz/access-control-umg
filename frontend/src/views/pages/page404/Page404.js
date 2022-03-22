a533c1103145a4dac6f9cbb25a10712f7b670746
Isaac Espinoza <isaackez@outlook.com>
2022-03-22 13:37:53 -0600

    cleanup

--- frontend/src/views/pages/page404/Page404.js
+++ frontend/src/views/pages/page404/Page404.js
@@ -1,15 +1,5 @@
 import React from 'react'
-import {
-  CButton,
-  CCol,
-  CContainer,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilMagnifyingGlass } from '@coreui/icons'
+import { CCol, CContainer, CRow } from '@coreui/react'
 
 const Page404 = () => {
   return (
@@ -19,18 +9,11 @@ const Page404 = () => {
           <CCol md={6}>
             <div className="clearfix">
               <h1 className="float-start display-3 me-4">404</h1>
-              <h4 className="pt-3">Oops! You{"'"}re lost.</h4>
+              <h4 className="pt-3">Oops! Pareces perdido</h4>
               <p className="text-medium-emphasis float-start">
-                The page you are looking for was not found.
+                No encontamos la pagina que buscas.
               </p>
             </div>
-            <CInputGroup className="input-prepend">
-              <CInputGroupText>
-                <CIcon icon={cilMagnifyingGlass} />
-              </CInputGroupText>
-              <CFormInput type="text" placeholder="What are you looking for?" />
-              <CButton color="info">Search</CButton>
-            </CInputGroup>
           </CCol>
         </CRow>
       </CContainer>
