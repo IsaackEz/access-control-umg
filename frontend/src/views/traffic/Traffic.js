73e250204cd74e40ef06f3f42e1f61a7289d2096
Isaac Espinoza <isaackez@outlook.com>
2022-03-22 13:37:35 -0600

    quick template

---
+++ frontend/src/views/traffic/Traffic.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import { CCard, CCardBody, CRow } from '@coreui/react'
+
+const Traffic = () => {
+  return (
+    <CRow>
+      <CCard className=" mb-4"></CCard>
+    </CRow>
+  )
+}
+
+export default Traffic
---
+++ frontend/src/views/users/Users.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
+
+import { CCard, CCardBody, CRow } from '@coreui/react'
+import DataTable from 'react-data-table-component'
+import { io } from 'socket.io-client'
+
+const Tables = () => {
+  const [userData, setUserData] = useState([])
+  const [recordsAll, setRecordsAll] = useState([])
+
+  const socket = useRef(io.connect('http://localhost:5000/'))
+
+  const loadUserData = async () => {
+    await axios
+      .get('https://api.cinic.xyz/api/users')
+      .then((res) => {
+        setUserData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  const loadAllRecords = async () => {
+    await axios
+      .get('https://api.cinic.xyz/api/records')
+      .then((res) => {
+        setRecordsAll(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    loadAllRecords()
+    loadUserData()
+    socket.current.on('newUser', () => {
+      loadAllRecords()
+      loadUserData()
+    })
+  }, [])
+
+  const columns = [
+    {
+      name: 'Nombre',
+      selector: (row) => row.fullName,
+      sortable: true,
+    },
+    {
+      name: 'Usuario',
+      selector: (row) => row.userID,
+      sortable: true,
+    },
+    {
+      name: 'Rol',
+      selector: (row) => row.userRol,
+      sortable: true,
+    },
+    {
+      name: 'Estado',
+      selector: (row) => row.checkInPlace,
+      sortable: true,
+    },
+    {
+      name: 'Sintomas',
+      selector: (row) => row.checkInTime,
+      sortable: true,
+    },
+    {
+      name: 'Ultimo acceso',
+      selector: (row) => row.checkOutPlace,
+      sortable: true,
+    },
+    {
+      name: 'Bloquear',
+      selector: (row) => row.checkOutTime,
+      sortable: true,
+    },
+  ]
+
+  const getNames = () => {
+    let fullRecords = recordsAll.slice()
+    userData.forEach((names) => {
+      fullRecords.forEach((record) => {
+        if (names.userID === record.userID) {
+          record['fullName'] = names.name + ' ' + names.lastname
+          record['userRol'] = names.userRol
+        }
+      })
+    })
+    return fullRecords
+  }
+
+  return (
+    <CRow>
+      <CCard className=" mb-4">
+        <CCardBody>
+          <h5 className="card-title mb-2">Historial</h5>
+          <hr className="mt-0" />
+          <div className="containerTable">
+            <DataTable columns={columns} data={getNames()} pagination rowsPerPage={15} responsive />
+          </div>
+        </CCardBody>
+      </CCard>
+    </CRow>
+  )
+}
+
+export default Tables
