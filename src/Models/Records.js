6437bf0b67297afea3f7a093d45c86f8674ae436
Isaac Espinoza <isaackez@outlook.com>
2022-05-18 21:55:19 -0500

    + timestamp for sort

--- src/Models/Records.js
+++ src/Models/Records.js
@@ -1,24 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const RecordSchema = new Schema({
-	userID: { type: String, required: true, trim: true },
-	userRol: { type: String, trim: true },
-	checkInPlace: { type: String, required: true, trim: true },
-	checkInTime: { type: Date, required: true, trim: true },
-	checkOutPlace: { type: String, trim: true },
-	checkOutTime: { type: Date, trim: true },
-	usersAfter: { type: Number, trim: true },
-	usersBefore: { type: Number, trim: true },
-	records: [
-		{
-			recordInPlace: { type: String, trim: true },
-			recordInTime: { type: Date, trim: true },
-			recordOutTime: { type: Date, trim: true },
-			usersAfter: { type: Number, trim: true },
-			usersBefore: { type: Number, trim: true },
-		},
-	],
-});
+const RecordSchema = new Schema(
+	{
+		userID: { type: String, required: true, trim: true },
+		userRol: { type: String, trim: true },
+		checkInPlace: { type: String, required: true, trim: true },
+		checkInTime: { type: Date, required: true, trim: true },
+		checkOutPlace: { type: String, trim: true },
+		checkOutTime: { type: Date, trim: true },
+		usersAfter: { type: Number, trim: true },
+		usersBefore: { type: Number, trim: true },
+		records: [
+			{
+				recordInPlace: { type: String, trim: true },
+				recordInTime: { type: Date, trim: true },
+				recordOutTime: { type: Date, trim: true },
+				usersAfter: { type: Number, trim: true },
+				usersBefore: { type: Number, trim: true },
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 module.exports = Record = mongoose.model('records', RecordSchema);
